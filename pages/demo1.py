f78fb6a104a36b27d23a7658d71d71d8ed446e14
sha1-1 <84086478+sha1-1@users.noreply.github.com>
2022-12-04 18:01:29 +0800

    Add files via upload

---
+++ pages/demo1.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+# pip install pandas -i https://mirrors.aliyun.com/pypi/simple
+# pip install -U requests[security]
+""" my_wx: windforyou_ """
+import streamlit as st
+
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
